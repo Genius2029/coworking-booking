9e65d2f79f0906ffc7e6388f9e12245f42c84cdf
NicolasJackson <nikolajun8@gmail.com>
2026-08-27 21:00:53 +0900

    owner/user security system is working

--- app/routers/coworking.py
+++ app/routers/coworking.py
@@ -1,22 +1,31 @@
 from datetime import datetime
 from sqlmodel import select
-from fastapi import APIRouter , Depends
+from fastapi import APIRouter , Depends, HTTPException
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 from app.database import get_session
 from app.models.coworking import Coworking
 from app.schemas.coworking import CoworkingCreate
+from app.core.security import get_current_user
+from app.models.user import User
 
 router = APIRouter()
 
 
 @router.post("/coworking")
-async def create_coworking(data: CoworkingCreate, session: AsyncSession = Depends(get_session)):
+async def create_coworking(
+    data: CoworkingCreate,
+    session: AsyncSession = Depends(get_session),
+    current_user: User = Depends(get_current_user)
+):
+    if current_user.role != "owner":
+        raise HTTPException(status_code=403, detail="Only owners can create coworking")
+    
     new_coworking = Coworking(
         name=data.name,
         address=data.address, 
         description=data.description,
-        owner_id=1,
+        owner_id=current_user.id,
         created_at=str(datetime.now())
         )
     session.add(new_coworking)
